c0e1cf0225f5b692fd3e846e0a790d5d21b404bf
Fran <98226655+P4NCH1T0@users.noreply.github.com>
2025-02-28 13:39:25 -0600

    Creacion del esqueleto

---
+++ crud/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h5>React CRUD usando PHP API y MySQL</h5>
+      <BrowserRouter>
+        <nav>
+          <ul>
+            <li>
+              <Link to></Link>
+            </li>
+          </ul>
+        </nav>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ crud/src/Components/ListUser.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ListUser() {
+  return (
+    <div>List User</div>
+  )
+}
+
+export default ListUser
